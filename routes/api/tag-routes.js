f3cd7996a031c15c67733042ff4c12213ba53981
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2021-12-14 22:57:13 -0800

    Added routes for new, updating, and deleting tags.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   try {
     const allTag = await Tag.findAll({include: [{ model: Product }]})
     if (!allTag) {
-      res.status(404).json({ message: `The database currently has no tags.` })
+      res.status(404).json({message: `The database currently has no tags.`})
       return
     }
     res.status(200).json(allTag)
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
   try {
     const oneTag = await Tag.findByPk(req.params.id, {include: [{ model: Product }]})
     if (!oneTag) {
-      res.status(404).json({ message: `There are no tags with this ID in the database.` })
+      res.status(404).json({message: `There are no tags with this ID in the database.`})
       return
     }
     res.status(200).json(oneTag)
@@ -33,14 +33,42 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body)
+    res.status(200).json(newTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(
+      { tag_name: req.body.tag_name },
+      { where: { id: req.params.id } })
+    if (!updateTag) {
+      res.status(404).json({message: 'Tag does not exist.'})
+      return
+    }
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({where: { id: req.params.id }})
+    if (!deleteTag) {
+      res.status(404).json({message: `Entered tag does not exist in the database.`})
+      return
+    }
+    res.status(200).json({message: `Tag deleted from the database.`})
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
